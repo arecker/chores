b216af89474bae9cf29c2f3234f307252af34d08
Alex Recker <alex@reckerfamily.com>
2020-06-12 15:43:28 -0500

    MORE SHIT

--- static/app.js
+++ static/app.js
@@ -1,6 +1,34 @@
+var Chores = (function() {
+  var api = {};
+
+  api.list = function() {
+    return axios.get('http://localhost:5000/api/chores/')
+  };
+
+  return api;
+}());
+
 var app = new Vue({
   el: '#app',
+
   data: {
-    message: 'Hello Vue!'
+    chores: [],
+  },
+
+  mounted: function() {
+    var app = this;
+    
+    Chores.list().then(function(response) {
+      app.chores = response.data;
+    });
+  },
+
+  filters: {
+    assignee: function(value) {
+      return {
+	'0': 'Alex',
+	'1': 'Marissa',
+      }[value + ''] || 'Unknown';
+    }
   }
 });
